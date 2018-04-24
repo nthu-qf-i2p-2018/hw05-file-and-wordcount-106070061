169921a88243494787d32fe218284f4261987f4f
106070061 <duclap19990919@gmail.com>
2018-04-24 23:52:26 +0800

    1

--- hw5.py
+++ hw5.py
@@ -1,48 +1,31 @@
-# -*- coding: utf-8 -*-
-import ...
-
+import csv
+import string
+import json
+import pickle
 
 def main(filename):
-    # read file into lines
-    lines = ...
-
-    # declare a word list
+    file=open(filename)
+    lines = file.readlines()
     all_words = []
 
-    # extract all words from lines
     for line in lines:
-        # split a line of text into a list words
-        # "I have a dream." => ["I", "have", "a", "dream."]
-        words = ...
-
-        # check the format of words and append it to "all_words" list
+        words =line.split()
+        
         for word in words:
-            # then, remove (strip) unwanted punctuations from every word
-            # "dream." => "dream"
-            word = ...
-            # check if word is not empty
+            word = word.strip(string.punctuation)
             if word:
-                # append the word to "all_words" list
-                all_words...
+                all_words.append(word)
+                
+    from collections import Counter
+    word_counter = Counter(all_words)
 
-    # compute word count from all_words
-    counter = ...
-
-    # dump to a csv file named "wordcount.csv":
-    # word,count
-    # a,12345
-    # I,23456
-    # ...
-    with open(...) as csv_file:
-        # create a csv writer from a file object (or descriptor)
-        writer = ...
-        # write table head
+    with open("word_count.csv", "w") as csv_file:
+        writer =csv.writer(csv_file)
         writer.writerow(['word', 'count'])
-        # write all (word, count) pair into the csv writer
-        writer.writerows(...)
+        writer.writerows(word_counter.most_common())
 
-    # dump to a json file named "wordcount.json"
-    ...
+    json.dump(word_counter,open('word_count.json','w'))
+    pickle.dump(word_counter,open('word_count.pkl','wb'))
 
     # BONUS: dump to a pickle file named "wordcount.pkl"
     # hint: dump the Counter object directly
